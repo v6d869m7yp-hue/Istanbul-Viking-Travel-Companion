acb44d8bddcdc65d7b2f9ce618df25074c7cf88d
v6d869m7yp-hue <v6d869m7yp@privaterelay.appleid.com>
2026-07-27 19:50:47 -0700

    Fix nonfunctional schematic map hotspots

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE='ivtc-v3.5.1';
+const CACHE='ivtc-v3.5.2';
 const CORE=[
   "./.gitignore",
   "./BOSPHORUS-MAP-FIX.txt",
   "./DUBROVNIK-PHOTO-FIX.txt",
   "./README.md",
   "./INTERACTIVE-CRUISE-MAP.txt",
+  "./INTERACTIVE-MAPS-FIX.txt",
   "./TROY-MAP-FIX.txt",
   "./TROY-PORT-PHOTO-FIX.txt",
   "./VERSION.txt",
